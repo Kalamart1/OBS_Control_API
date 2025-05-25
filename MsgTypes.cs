2e12fb21dbd2935ed1fd5c6ac0b23acaf3a60ad4
Kalamart <kalamart1@yahoo.com>
2025-05-25 19:21:06 +0200

    Websocket client with hardcoded parameters

---
+++ MsgTypes.cs
@@ -0,0 +1,101 @@
+﻿namespace OBS_Control_API
+{
+    /**
+     * <summary>
+     * Exhaustive list of Op codes in the OBS websocket API.
+     * </summary>
+     */
+    public enum OpCode
+    {
+        Hello = 0,
+        Identify = 1,
+        Identified = 2,
+        Reidentify = 3,
+        Event = 5,
+        Request = 6,
+        RequestResponse = 7,
+        RequestBatch = 8,
+        RequestBatchResponse = 9
+    }
+
+    /**
+     * <summary>
+     * Structure of received messages.
+     * </summary>
+     */
+    public class Msg
+    {
+        public object d { get; set; }
+        public int op { get; set; }
+    }
+
+    /**
+     * <summary>
+     * Structure of the data received with OpCode Hello.
+     * </summary>
+     */
+    public class Hello
+    {
+        public Authentication authentication { get; set; }
+        public string obsWebSocketVersion { get; set; }
+        public string rpcVersion { get; set; }
+        public class Authentication
+        {
+            public string challenge { get; set; }
+            public string salt { get; set; }
+        }
+    }
+
+    /**
+     * <summary>
+     * Structure of the data received with OpCode RequestResponse.
+     * This is for parsing purposes only, and will not be directly transmitted to the caller.
+     * </summary>
+     */
+    public class RequestResponseRaw
+    {
+        public string requestId { get; set; }
+        public RequestResponse.RequestStatus requestStatus { get; set; }
+        public object responseData { get; set; } // optional, depends on request
+    }
+
+    /**
+     * <summary>
+     * Structure of the data of the request response that we want to keep.
+     * </summary>
+     */
+    public partial class RequestResponse
+    {
+        public RequestStatus requestStatus { get; set; }
+        public string responseData { get; set; } // optional, depends on request
+        public class RequestStatus
+        {
+            public bool result { get; set; }
+            public int code { get; set; }
+            public string comment { get; set; } // optional
+        }
+
+        /**
+         * <summary>
+         * Non-exhaustive list of codes that can be returned in RequestStatus
+         * </summary>
+         */
+        public enum Code
+        {
+            Success = 100,
+            NotReady = 207
+        }
+    }
+
+    /**
+     * <summary>
+     * Structure of the data received with OpCode Event.
+     * </summary>
+     */
+    public partial class Event
+    {
+        public object eventData { get; set; } // depends on event
+        public int eventIntent { get; set; }
+        public string eventType { get; set; }
+    }
+}
--- main.cs
+++ main.cs
@@ -10,19 +10,180 @@ namespace OBS_Control_API
         public const string Author = "Kalamart";
         public const string Company = "";
     }
-
-    public class MyMod : MelonMod
+    public partial class OBS : MelonMod
     {
-        public static void Log(string msg)
+        //constants
+        private bool forceReplayBuffer = true;
+        private const string ip = "localhost";
+        private const int port = 4455;
+        private const string password = "your_password_here";
+
+        // variables
+        private ConnectionManager connectionManager;
+        private static RequestManager requestManager;
+
+        private bool isReplayBufferActive = false;
+        private bool isRecordingActive = false;
+        private bool isStreamActive = false;
+        private bool stopReplayBufferAtShutdown = false;
+
+        /**
+         * <summary>
+         * Log to console.
+         * </summary>
+         */
+        private static void Log(string msg)
         {
             MelonLogger.Msg(msg);
         }
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        /**
+         * <summary>
+         * Log to console but in yellow.
+         * </summary>
+         */
+        private static void LogWarn(string msg)
+        {
+            MelonLogger.Warning(msg);
+        }
+        /**
+         * <summary>
+         * Log to console but in red.
+         * </summary>
+         */
+        private static void LogError(string msg)
+        {
+            MelonLogger.Error(msg);
+        }
+
+        /**
+         * <summary>
+         * Called when the mod is initialized at the start of the game.
+         * </summary>
+         */
+        public override void OnLateInitializeMelon()
+        {
+            requestManager = new RequestManager();
+            connectionManager = new ConnectionManager(requestManager, ip, port, password);
+            InitEvents();
+            onConnect += OnConnect;
+            onDisconnect += OnDisconnect;
+            connectionManager.Start();
+        }
+
+        /**
+         * <summary>
+         * (Re)start connection thread.
+         * </summary>
+         */
+        public void Connect()
+        {
+            if (!IsConnected())
+            {
+                connectionManager.UpdateWebsocketConfig(ip, port, password);
+                connectionManager.Start();
+            }
+        }
+
+        /**
+         * <summary>
+         * Close the connection and don't try to reconnect.
+         * </summary>
+         */
+        public void Disconnect()
+        {
+            connectionManager.Stop();
+        }
+
+        /**
+         * <summary>
+         * Called when the connection with OBS is 100% established and authentication is complete.
+         * </summary>
+         */
+        private void OnConnect()
+        {
+            SetMainStatus();
+            if (!isReplayBufferActive && forceReplayBuffer)
+            {
+                Log($"Replay Buffer was inactive, starting it...");
+                stopReplayBufferAtShutdown = true;
+                StartReplayBuffer();
+            }
+        }
+
+        /**
+         * <summary>
+         * Called when the connection with OBS is closed.
+         * </summary>
+         */
+        private void OnDisconnect()
+        {
+            isReplayBufferActive = false;
+            isRecordingActive = false;
+            isStreamActive = false;
+        }
+
+        /**
+         * <summary>
+         * Called 50 times per second, used for frequent updates.
+         * </summary>
+         */
+        public override void OnFixedUpdate()
         {
         }
 
-        public override void OnFixedUpdate()
+        /**
+         * <summary>
+         * Called when the game is closed cleanly (by closing the window).
+         * </summary>
+         */
+        public override void OnApplicationQuit()
         {
+            if (connectionManager.IsConnected() && isReplayBufferActive && stopReplayBufferAtShutdown)
+            {
+                Log("Stopping replay buffer");
+                StopReplayBuffer();
+            }
+            connectionManager.Stop();
+        }
+
+        /**
+         * <summary>
+         * Returns true if the client is connected to OBS, ready to send requests and receive events.
+         * </summary>
+         */
+        public bool IsConnected()
+        {
+            return connectionManager.IsConnected();
+        }
+
+        /**
+         * <summary>
+         * Returns true if the replay buffer is active.
+         * </summary>
+         */
+        public bool IsReplayBufferActive()
+        {
+            return isReplayBufferActive;
+        }
+
+        /**
+         * <summary>
+         * Returns true if recording is active.
+         * </summary>
+         */
+        public bool IsRecordingActive()
+        {
+            return isRecordingActive;
+        }
+
+        /**
+         * <summary>
+         * Returns true if streaming is active.
+         * </summary>
+         */
+        public bool IsStreamActive()
+        {
+            return isStreamActive;
         }
     }
 }
